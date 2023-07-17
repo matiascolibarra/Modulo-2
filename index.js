6ffac56981d3957edea7c99519b775e49a9b8c91
matiascolibarra <133715601+matiascolibarra@users.noreply.github.com>
2023-07-17 20:14:48 -0300

    update

---
+++ index.js
@@ -0,0 +1,16 @@
+/* Tres Maneras de declarar una variable en JS */
+//variable
+var uno = 12;
+//variable de bloques { }
+let dos = 'hola';
+// variable const 
+const tres = false;
+
+//typeof
+console.log(typeof dos);
+console.log(typeof uno);
+console.log(typeof tres);
+
+let x = 34;
+
+console.log(uno-=x);
